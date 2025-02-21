b236614d5bb114cdf3d6a628ed4abbfcacb45dcb
dinesh-tech-757 <dineshmoorthi757@gmail.com>
2025-02-21 11:47:40 +0530

    forgot password-5

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -327,8 +327,8 @@ router.post("/forgot-password", async (req, res) => {
         const transporter = nodemailer.createTransport({
             service: "Gmail",
             auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
+                email: process.env.EMAIL_USER,
+                password: process.env.EMAIL_PASS,
             },
         });
 
